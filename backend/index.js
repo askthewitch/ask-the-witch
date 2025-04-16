60eddfa2a34a97a313638ad99327a77904da8d22
Thomas Hughes <thomashughes@Thomass-Mac-mini.local>
2025-04-16 17:13:40 +0700

    Backend: Update CORS to include Vercel origin(s)

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,11 @@ require("dotenv").config();
 const app = express();
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-
 const allowedOrigins = [
   "http://localhost:3000",
   "https://askthewitch.com",
   "https://www.askthewitch.com",
   "https://ask-the-witch.vercel.app",
-
 ];
 
 app.use(cors({
@@ -32,9 +30,23 @@ app.use(cors({
 app.use(bodyParser.json());
 
 app.post("/api/send-email", async (req, res) => {
+  try {
+    const { to, subject, html } = req.body;
+    const data = await resend.emails.send({
+      from: "Ask the Witch <noreply@askthewitch.com>",
+      to: to,
+      subject: subject,
+      html: html,
+    });
 
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin); // <---- TRY THIS
+    res.status(200).json({ success: true, data });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin); // <---- AND THIS
+    res.status(500).json({ success: false, error: error.message });
+  }
 });
-
 
 app.get("/api/", (req, res) => {
   res.send("Ask the Witch backend is working! 🧙‍♀️");
