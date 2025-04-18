46b2d9c869227c4ad06cb04304bdf50eb8b4d4d0
Thomas Hughes <thomashughes@Thomass-Mac-mini.local>
2025-04-18 14:50:01 +0700

    Frontend: Set background image via inline style in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import "./style.css";
-import "./App.css";
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Loading from "./pages/Loading";
-import Results from "./pages/Results";
+import './style.css';
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Loading from './pages/Loading';
+import Results from './pages/Results';
 import PrivacyTerms from './pages/PrivacyTerms';
 import AboutWitch from './pages/AboutWitch';
 import TermsOfService from './pages/TermsOfService';
+import backgroundImage from '../public/images/backgroundthewitch.png'; // Import the image
 
 function App() {
   return (
     <Router>
-      <div className="page-wrapper">
+      <div className="page-wrapper" style={{ backgroundImage: `url(${backgroundImage})`, backgroundRepeat: 'repeat', backgroundSize: 'auto', backgroundAttachment: 'fixed' }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/loading" element={<Loading />} />
           <Route path="/results" element={<Results />} />
-          <Route path="/privacy" element={<PrivacyTerms />} /> {/* Add this route */}
-          <Route path="/terms" element={<TermsOfService />} /> {/* Add this route */}
-          <Route path="/about" element={<AboutWitch />} />   {/* Add this route */}
+          <Route path="/privacy" element={<PrivacyTerms />} />
+          <Route path="/terms" element={<TermsOfService />} />
+          <Route path="/about" element={<AboutWitch />} />
         </Routes>
       </div>
     </Router>
