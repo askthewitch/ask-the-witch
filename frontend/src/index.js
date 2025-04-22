8f1380db42c69c47c61177e3a7bf6264073ef85f
Thomas Hughes <thomashughes@Thomass-Mac-mini.local>
2025-04-22 12:48:52 +0700

    Frontend(index): Wrap App with BrowserRouter and HelmetProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { BrowserRouter } from 'react-router-dom'; 
+import { HelmetProvider } from 'react-helmet-async'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// Set background image using JavaScript
+
 document.body.style.backgroundImage = `url(${process.env.PUBLIC_URL}/images/backgroundthewitch.png)`;
 document.body.style.backgroundRepeat = 'repeat';
 document.body.style.backgroundSize = 'auto';
-document.body.style.backgroundAttachment = 'fixed'; // Optional
+document.body.style.backgroundAttachment = 'fixed'; 
