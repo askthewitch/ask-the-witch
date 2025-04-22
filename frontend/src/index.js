f99265ea9ae71e1c6329cf0282a469b44c78d69c
Thomas Hughes <thomashughes@Thomass-Mac-mini.local>
2025-04-22 13:15:47 +0700

    Frontend(index): Remove redundant BrowserRouter to fix routing error

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,22 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; 
-import { HelmetProvider } from 'react-helmet-async'; 
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </BrowserRouter>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
-
 
 document.body.style.backgroundImage = `url(${process.env.PUBLIC_URL}/images/backgroundthewitch.png)`;
 document.body.style.backgroundRepeat = 'repeat';
 document.body.style.backgroundSize = 'auto';
-document.body.style.backgroundAttachment = 'fixed'; 
+document.body.style.backgroundAttachment = 'fixed';
